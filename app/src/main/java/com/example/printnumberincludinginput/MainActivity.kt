069c5f0cc7701ab71aaa36d4747a9d022e0592d9
Noorish <noorish.shaikh@caperminttechnologies.com>
2022-06-28 18:50:44 +0530

    printnumberincludinginput

---
+++ app/src/main/java/com/example/printnumberincludinginput/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.printnumberincludinginput
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var txtInput: TextView
+    lateinit var button: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button=findViewById(R.id.btn)
+        txtInput=findViewById(R.id.et_number)
+
+        button.setOnClickListener {
+            val inputNumber = txtInput.text.toString().toInt()
+          //println(inputNumber)
+            if (inputNumber < 0 || inputNumber > 100) {
+               println(inputNumber)
+
+            } else {
+                for (i in inputNumber..(inputNumber + 10)) {
+                 println(i)
+                }
+
+            }
+        }}}
+
+
+
+
+
+
+
